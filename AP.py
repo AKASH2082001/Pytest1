1386e9a64b0247a0a6757931ce8ebed271fb1487
AKASH2082001 <akashbme208@gmail.com>
2022-03-28 23:52:59 -0700

    pytest1

--- AP.py
+++ AP.py
@@ -1,20 +1,50 @@
 def Area_of_square(sides):
-    return sides**2
+    return sides ** 2
 
 def perimeter_of_rectangle(length,breadth):
     return 2*(length+breadth)
 
-def Area_of_Rectangle(length,breadth):
-    return length*breadth
+def Area_of_Rectangle(length, breadth):
+     return length * breadth
+
 
 if __name__ == "__main__":
-    sides = int(input("enter the value of side: "))
-    length = int(input("enter the value og length: "))
-    breadth = int(input("enter the value of breadth: "))
+    while True:
+        print("select an option")
+        print("1. Area of square")
+        print("2.Perimeter of Rectangle")
+        print("3. Area of Rectangle")
+        print("4.Exit")
 
-    areaofsquare = Area_of_square(sides)
-    print(areaofsquare)
-    perimeterofrectangle = perimeter_of_rectangle(length,breadth)
-    print(perimeterofrectangle)
-    areaofrectangle = Area_of_Rectangle(length,breadth)
-    print(areaofrectangle)
+        choice = int(input("enter the option: "))
+
+        if choice == 1:
+            sides = int(input("enter the value of side: "))
+            areaofsquare = Area_of_square(sides)
+            print(areaofsquare)
+
+        elif choice == 2:
+
+
+            length = int(input("enter the value og length: "))
+            breadth = int(input("enter the value of breadth: "))
+            perimeterofrectangle = perimeter_of_rectangle(length, breadth)
+            print(perimeterofrectangle)
+
+        elif choice == 3:
+
+
+            length = int(input("enter the value og length: "))
+            breadth = int(input("enter the value of breadth: "))
+            areaofrectangle = Area_of_Rectangle(length, breadth)
+            print(areaofrectangle)
+
+        elif choice == 4:
+            break
+
+        else:
+            print("invalid option")
+
+
+
+
--- test_area.py
+++ test_area.py
@@ -1,19 +1,17 @@
 import AP
 
-import pytest
 
-@pytest.mark.areasqarearectperirect
 def test_Area_of_square():
     sides= 4
     result = AP.Area_of_square(sides)
     assert result == sides**2
-@pytest.mark.areasqarearectperirect
+
 def test_perimeter_of_rectangle():
     length = 10
     breadth = 2
     result = AP.perimeter_of_rectangle(length,breadth)
     assert result == 2*(length+breadth)
-@pytest.mark.areasqarearectperirect
+
 def test_Area_of_Rectangle():
     length = 10
     breadth = 2
